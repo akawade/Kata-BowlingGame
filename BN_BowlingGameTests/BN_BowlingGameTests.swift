d943eda3dd25a1bf9e2fdfb1b11e30730324ade7
Avinash Repositries <avinashkawade@gmail.com>
2022-11-25 12:06:59 +0530

    refactor for loop for few testcase

--- BN_BowlingGameTests/BN_BowlingGameTests.swift
+++ BN_BowlingGameTests/BN_BowlingGameTests.swift
@@ -1,14 +1,14 @@
-//
-//  BN_BowlingGameTests.swift
-//  BN_BowlingGameTests
-//
-//  Created by Avinash Kawade on 25/11/22.
-//
+    //
+    //  BN_BowlingGameTests.swift
+    //  BN_BowlingGameTests
+    //
+    //  Created by Avinash Kawade on 25/11/22.
+    //
 
-import XCTest
-@testable import BN_BowlingGame
+    import XCTest
+    @testable import BN_BowlingGame
 
-class BN_BowlingGameTests: XCTestCase {
+    class BN_BowlingGameTests: XCTestCase {
     let game = BlowingGame()
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,71 +20,69 @@ class BN_BowlingGameTests: XCTestCase {
 
     func testGame_AllRollZeroPins()
     {
-  
-        for _ in 1...20 {
-            game.roll(pins:0)
-        }
-        
-        let score = game.score()
-        XCTAssertEqual(score, 0)
+        roll(num: 0)
+        XCTAssertEqual(game.score(), 0)
     }
-    
-//    func testGame_SomeRollPinsHit()
-//    {
-//        game.roll(pins:0)
-//        game.roll(pins:1)
-//
-//        game.roll(pins:5)
-//        game.roll(pins:3)
-//
-//        game.roll(pins:2)
-//        game.roll(pins:3)
-//
-//        game.roll(pins:0)
-//        game.roll(pins:0)
-//
-//        game.roll(pins:0)
-//        game.roll(pins:0)
-//
-//        game.roll(pins:0)
-//        game.roll(pins:0)
-//
-//        game.roll(pins:0)
-//        game.roll(pins:0)
-//
-//        game.roll(pins:0)
-//        game.roll(pins:0)
-//
-//        game.roll(pins:0)
-//        game.roll(pins:0)
-//
-//        game.roll(pins:0)
-//        game.roll(pins:5)
-//
-//
-//        let score = game.score()
-//        XCTAssertEqual(score, 19)
-//    }
-    
+
+    //    func testGame_SomeRollPinsHit()
+    //    {
+    //        game.roll(pins:0)
+    //        game.roll(pins:1)
+    //
+    //        game.roll(pins:5)
+    //        game.roll(pins:3)
+    //
+    //        game.roll(pins:2)
+    //        game.roll(pins:3)
+    //
+    //        game.roll(pins:0)
+    //        game.roll(pins:0)
+    //
+    //        game.roll(pins:0)
+    //        game.roll(pins:0)
+    //
+    //        game.roll(pins:0)
+    //        game.roll(pins:0)
+    //
+    //        game.roll(pins:0)
+    //        game.roll(pins:0)
+    //
+    //        game.roll(pins:0)
+    //        game.roll(pins:0)
+    //
+    //        game.roll(pins:0)
+    //        game.roll(pins:0)
+    //
+    //        game.roll(pins:0)
+    //        game.roll(pins:5)
+    //
+    //
+    //        let score = game.score()
+    //        XCTAssertEqual(score, 19)
+    //    }
+
     func testGame_AllRollPinsHitThree()
     {
-        for _ in 1...20 {
-            game.roll(pins:2)
-        }
-        let score = game.score()
-        XCTAssertEqual(score, 40)
+        roll(num: 2)
+        XCTAssertEqual(game.score(), 40)
     }
-    
+
     func testGame_RollHitNineAndZeroPins()
     {
         for _ in 1...10 {
             game.roll(pins:9)
             game.roll(pins:0)
         }
-        let score = game.score()
-        XCTAssertEqual(score, 90)
+        XCTAssertEqual(game.score(), 90)
     }
-    
+
+    private func roll(num: Int){
+        
+        for _ in 1...20 {
+            game.roll(pins:num)
+        }
+    }
+
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -96,5 +94,4 @@ class BN_BowlingGameTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
