67bf70c2fd439c86cc1a41588cd4ddc83e4ee9cc
Avinash Repositries <avinashkawade@gmail.com>
2022-11-25 13:00:26 +0530

    added testcase for AnyFrameSpareExceptLast

--- BN_BowlingGame/BlowingGame.swift
+++ BN_BowlingGame/BlowingGame.swift
@@ -11,15 +11,33 @@ class BlowingGame {
     
     var rolls = [Int]()
     var scoreCount = 0
+    var spareBonus = 0
+    var frameScore = 0
     
     func roll(pins: Int) {
         rolls.append(pins)
     }
     
     func score() -> Int {
-        for i in 0...rolls.count-1 {
-            scoreCount += rolls[i]
+        //for i in 0...rolls.count-1 {
+        for i in stride(from: 0, to: rolls.count-1, by: 2) {
+            print(i)
+       
+            if rolls.count >= 2 {
+                if rolls[i] + rolls[i + 1]  == 10 {
+                  // its is spare frame
+                    spareBonus += rolls[i + 2]
+                }
+            }
+            frameScore = rolls[i] + rolls[i + 1]
+            print("frameScore  == \(frameScore)")
+            scoreCount +=  frameScore
+            print("scoreCount  == \(scoreCount)")
+
         }
+        scoreCount += spareBonus
+        print("Total scoreCount  == \(scoreCount)")
+
         return scoreCount
     }
 }
--- BN_BowlingGameTests/BN_BowlingGameTests.swift
+++ BN_BowlingGameTests/BN_BowlingGameTests.swift
@@ -4,94 +4,110 @@
     //
     //  Created by Avinash Kawade on 25/11/22.
     //
-
+    
     import XCTest
     @testable import BN_BowlingGame
-
+    
     class BN_BowlingGameTests: XCTestCase {
-    let game = BlowingGame()
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testGame_AllRollZeroPins()
-    {
-        roll(num: 0)
-        XCTAssertEqual(game.score(), 0)
-    }
-
-    //    func testGame_SomeRollPinsHit()
-    //    {
-    //        game.roll(pins:0)
-    //        game.roll(pins:1)
-    //
-    //        game.roll(pins:5)
-    //        game.roll(pins:3)
-    //
-    //        game.roll(pins:2)
-    //        game.roll(pins:3)
-    //
-    //        game.roll(pins:0)
-    //        game.roll(pins:0)
-    //
-    //        game.roll(pins:0)
-    //        game.roll(pins:0)
-    //
-    //        game.roll(pins:0)
-    //        game.roll(pins:0)
-    //
-    //        game.roll(pins:0)
-    //        game.roll(pins:0)
-    //
-    //        game.roll(pins:0)
-    //        game.roll(pins:0)
-    //
-    //        game.roll(pins:0)
-    //        game.roll(pins:0)
-    //
-    //        game.roll(pins:0)
-    //        game.roll(pins:5)
-    //
-    //
-    //        let score = game.score()
-    //        XCTAssertEqual(score, 19)
-    //    }
-
-    func testGame_AllRollPinsHitThree()
-    {
-        roll(num: 2)
-        XCTAssertEqual(game.score(), 40)
-    }
-
-    func testGame_RollHitNineAndZeroPins()
-    {
-        for _ in 1...10 {
-            game.roll(pins:9)
-            game.roll(pins:0)
+        let game = BlowingGame()
+        override func setUpWithError() throws {
+            // Put setup code here. This method is called before the invocation of each test method in the class.
         }
-        XCTAssertEqual(game.score(), 90)
-    }
-
-    private func roll(num: Int){
         
-        for _ in 1...20 {
-            game.roll(pins:num)
+        override func tearDownWithError() throws {
+            // Put teardown code here. This method is called after the invocation of each test method in the class.
+        }
+        
+        func testGame_AllRollZeroPins()
+        {
+            roll(num: 0)
+            XCTAssertEqual(game.score(), 0)
+        }
+        
+        //    func testGame_SomeRollPinsHit()
+        //    {
+        //        game.roll(pins:0)
+        //        game.roll(pins:1)
+        //
+        //        game.roll(pins:5)
+        //        game.roll(pins:3)
+        //
+        //        game.roll(pins:2)
+        //        game.roll(pins:3)
+        //
+        //        game.roll(pins:0)
+        //        game.roll(pins:0)
+        //
+        //        game.roll(pins:0)
+        //        game.roll(pins:0)
+        //
+        //        game.roll(pins:0)
+        //        game.roll(pins:0)
+        //
+        //        game.roll(pins:0)
+        //        game.roll(pins:0)
+        //
+        //        game.roll(pins:0)
+        //        game.roll(pins:0)
+        //
+        //        game.roll(pins:0)
+        //        game.roll(pins:0)
+        //
+        //        game.roll(pins:0)
+        //        game.roll(pins:5)
+        //
+        //
+        //        let score = game.score()
+        //        XCTAssertEqual(score, 19)
+        //    }
+        
+        func testGame_AllRollPinsHitThree()
+        {
+            roll(num: 2)
+            XCTAssertEqual(game.score(), 40)
+        }
+        
+        func testGame_RollHitNineAndZeroPins()
+        {
+            for _ in 1...10 {
+                game.roll(pins:9)
+                game.roll(pins:0)
+            }
+            XCTAssertEqual(game.score(), 90)
+        }
+        
+        func testGame_AnyFrameSpareExceptLastHitPins()
+        {
+            game.roll(pins:0)
+            game.roll(pins:0)
+            game.roll(pins:5)
+            game.roll(pins:5)
+            game.roll(pins:3)
+            game.roll(pins:6)
+            for _ in 1...7 {
+                game.roll(pins:0)
+                game.roll(pins:0)
+            }
+            
+            XCTAssertEqual(game.score(), 22)
+        }
+        
+        private func roll(num: Int){
+            
+            for _ in 1...20 {
+                game.roll(pins:num)
+            }
+        }
+        
+        func testExample() throws {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct results.
+        }
+        
+        func testPerformanceExample() throws {
+            // This is an example of a performance test case.
+            self.measure {
+                // Put the code you want to measure the time of here.
+            }
         }
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-}
